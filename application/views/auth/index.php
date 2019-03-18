8247a264c9db4c4d916aadb8ef303f3212d834d5
Muhammad Rihaz <muhammadrihazjr9@gmail.com>
2019-03-18 18:01:56 +0700

    start

---
+++ application/views/auth/index.php
@@ -0,0 +1,251 @@
+    <div class="main-header" style="background-image:url('<?= base_url('assets/img/Logo Palembang Kost.jpg'); ?>')">
+        <div class="search-container">
+            <div class="search-box">
+                <form class="search" action="" method="post">
+                    <div class="row no-gutters">
+                        <div class="col-10">
+                            <input type="text" placeholder="Cari Kost" class="form-control">
+                        </div>
+                        <div class="col-2">
+                            <button class="form-control  btn-tomato"><i class="fas fa-search"></i></button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <div class="univkost-container">
+        <div class="card-group">
+            <div class="card bg-dark text-white">
+                <img src="<?= base_url('assets/img/example2.jpg'); ?>" class="card-img" alt="...">
+                <a href="">
+                    <div class="card-img-overlay">
+                        <h5 class="card-title">KOST DEKAT UNIVERSITAS SRIWIJAYA</h5>
+                    </div>
+                </a>
+            </div>
+            <div class="card bg-dark text-white">
+                <img src="<?= base_url('assets/img/example2.jpg'); ?>" class="card-img" alt="...">
+                <a href="">
+                    <div class="card-img-overlay">
+                        <h5 class="card-title">KOST DEKAT UNIVERSITAS MUHAMMADIYAH</h5>
+                    </div>
+                </a>
+            </div>
+            <div class="card bg-dark text-white">
+                <img src="<?= base_url('assets/img/example2.jpg'); ?>" class="card-img" alt="...">
+                <a href="">
+                    <div class="card-img-overlay">
+                        <h5 class="card-title">KOST DEKAT UIN RADEN FATAH</h5>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="rekomen-container">
+        <div class="rekomen-title">
+            <h5>Rekomendasi Kost</h5>
+        </div>
+        <div class="kost-container">
+            <div class="row">
+                <div class="col-md-4 col-sm-6">
+                    <a href="test.php">
+                        <div class="kost">
+                            <div class="kost-img-box">
+                                <img src="<?= base_url('assets/img/example2.jpg'); ?>" class="kost-img" alt="">
+                                <div class="harga">
+                                    <span>Rp. 8.000.000/bulan</span>
+                                </div>
+                            </div>
+                            <div class="kost-desc">
+                                <div class="jenis">
+                                    <span>Campur</span>
+                                </div>
+                                <div class="kamar">
+                                    <span>Sisa 1 kamar</span>
+                                </div>
+                                <div class="namakost">
+                                    <h5>Pondok Abiqu Kost</h5>
+                                </div>
+                                <div class="lokasi">
+                                    <span class="lnr lnr-map-marker"></span>Jl. Padang Selasa, Bukit Lama, Ilir Barat I
+                                </div>
+                                <div class="fasilitas">
+                                    <p></p>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-4 col-sm-6">
+                    <a href="test.php">
+                        <div class="kost">
+                            <div class="kost-img-box">
+                                <img src="<?= base_url('assets/img/example2.jpg'); ?>" class="kost-img" alt="">
+                                <div class="harga">
+                                    <span>Rp. 8.000.000/bulan</span>
+                                </div>
+                            </div>
+                            <div class="kost-desc">
+                                <div class="jenis">
+                                    <span>Campur</span>
+                                </div>
+                                <div class="kamar">
+                                    <span>Sisa 1 kamar</span>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-4 col-sm-6">
+                    <a href="test.php">
+                        <div class="kost">
+                            <div class="kost-img-box">
+                                <img src="<?= base_url('assets/img/example2.jpg'); ?>" class="kost-img" alt="">
+                                <div class="harga">
+                                    <span>Rp. 8.000.000/bulan</span>
+                                </div>
+                            </div>
+                            <div class="kost-desc">
+                                <div class="jenis">
+                                    <span>Campur</span>
+                                </div>
+                                <div class="kamar">
+                                    <span>Sisa 1 kamar</span>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-4 col-sm-6">
+                    <a href="test.php">
+                        <div class="kost">
+                            <div class="kost-img-box">
+                                <img src="<?= base_url('assets/img/example2.jpg'); ?>" class="kost-img" alt="">
+                                <div class="harga">
+                                    <span>Rp. 8.000.000/bulan</span>
+                                </div>
+                            </div>
+                            <div class="kost-desc">
+                                <div class="jenis">
+                                    <span>Campur</span>
+                                </div>
+                                <div class="kamar">
+                                    <span>Sisa 1 kamar</span>
+                                </div>
+                                <div class="namakost">
+
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-4 col-sm-6">
+                    <a href="test.php">
+                        <div class="kost">
+                            <div class="kost-img-box">
+                                <img src="<?= base_url('assets/img/example2.jpg'); ?>" class="kost-img" alt="">
+                                <div class="harga">
+                                    <span>Rp. 8.000.000/bulan</span>
+                                </div>
+                            </div>
+                            <div class="kost-desc">
+                                <div class="jenis">
+                                    <span>Campur</span>
+                                </div>
+                                <div class="kamar">
+                                    <span>Sisa 1 kamar</span>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-4 col-sm-6">
+                    <a href="test.php">
+                        <div class="kost">
+                            <div class="kost-img-box">
+                                <img src="<?= base_url('assets/img/example2.jpg'); ?>" class="kost-img" alt="">
+                                <div class="harga">
+                                    <span>Rp. 8.000.000/bulan</span>
+                                </div>
+                            </div>
+                            <div class="kost-desc">
+                                <div class="jenis">
+                                    <span>Campur</span>
+                                </div>
+                                <div class="kamar">
+                                    <span>Sisa 1 kamar</span>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-4 col-sm-6">
+                    <a href="test.php">
+                        <div class="kost">
+                            <div class="kost-img-box">
+                                <img src="<?= base_url('assets/img/example2.jpg'); ?>" class="kost-img" alt="">
+                                <div class="harga">
+                                    <span>Rp. 8.000.000/bulan</span>
+                                </div>
+                            </div>
+                            <div class="kost-desc">
+                                <div class="jenis">
+                                    <span>Campur</span>
+                                </div>
+                                <div class="kamar">
+                                    <span>Sisa 1 kamar</span>
+                                </div>
+                                <div class="namakost">
+
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-4 col-sm-6">
+                    <a href="test.php">
+                        <div class="kost">
+                            <div class="kost-img-box">
+                                <img src="<?= base_url('assets/img/example2.jpg'); ?>" class="kost-img" alt="">
+                                <div class="harga">
+                                    <span>Rp. 8.000.000/bulan</span>
+                                </div>
+                            </div>
+                            <div class="kost-desc">
+                                <div class="jenis">
+                                    <span>Campur</span>
+                                </div>
+                                <div class="kamar">
+                                    <span>Sisa 1 kamar</span>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-4 col-sm-6">
+                    <a href="test.php">
+                        <div class="kost">
+                            <div class="kost-img-box">
+                                <img src="<?= base_url('assets/img/example2.jpg'); ?>" class="kost-img" alt="">
+                                <div class="harga">
+                                    <span>Rp. 8.000.000/bulan</span>
+                                </div>
+                            </div>
+                            <div class="kost-desc">
+                                <div class="jenis">
+                                    <span>Campur</span>
+                                </div>
+                                <div class="kamar">
+                                    <span>Sisa 1 kamar</span>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="load">
+                <a href="">+ Tampilkan lebih banyak</a>
+            </div>
+        </div>
+    </div> 
